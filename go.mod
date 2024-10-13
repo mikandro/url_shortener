02ed0e828a062f9bf5bb5293a62351dd51f986e5
mikandro <mihail.andritchi@gmail.com>
2024-10-13 13:09:24 +0200

    Add test for short url crud

--- go.mod
+++ go.mod
@@ -7,12 +7,15 @@ require (
 	github.com/alicebob/gopher-json v0.0.0-20200520072559-a9ecdc9d1d3a // indirect
 	github.com/alicebob/miniredis/v2 v2.33.0 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/go-chi/chi v1.5.5 // indirect
 	github.com/go-chi/chi/v5 v5.1.0 // indirect
 	github.com/go-chi/cors v1.2.1 // indirect
 	github.com/go-chi/render v1.0.3 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/redis/go-redis/v9 v9.6.1 // indirect
 	github.com/stretchr/testify v1.9.0 // indirect
 	github.com/yuin/gopher-lua v1.1.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -1,22 +1,50 @@
 package handlers
 
 import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
 	"github.com/alicebob/miniredis/v2"
-	"github.com/redis/go-redis/v9"
+	my_redis "github.com/mikandro/url_shortener/internal/redis"
+	"github.com/stretchr/testify/assert"
 )
 
-func setupTestRedis() (*redis.Client, func()) {
+func setupTestRedis() (*my_redis.Client, func()) {
 	s, err := miniredis.Run()
 	if err != nil {
 		panic(err)
 	}
 
-	client := redis.NewClient(&redis.Options{
-		Addr: s.Addr(),
-	})
+	client := my_redis.NewClient(s.Addr(), "", 1)
 
 	return client, func() {
 		client.Close()
 		s.Close()
 	}
+}
+
+func TestShortenUrl(t *testing.T) {
+	redisClient, teardown := setupTestRedis()
+	defer teardown()
+
+	handler := &UrlHandler{RedisClient: redisClient}
+
+	reqBody := `{"url": "https://example.com"}`
+	req := httptest.NewRequest(http.MethodPost, "/shorten", strings.NewReader(reqBody))
+	req.Header.Set("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+
+	handler.ShortenUrl(w, req)
+
+	res := w.Result()
+	defer res.Body.Close()
+
+	assert.Equal(t, http.StatusCreated, res.StatusCode)
+
+	var responseBody map[string]string
+	err := json.NewDecoder(res.Body).Decode(&responseBody)
+	assert.NoError(t, err)
 }

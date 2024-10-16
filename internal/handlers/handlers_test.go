ef9c6099358cb33f6f22febefd8e908d4d9b20b2
mikandro <mihail.andritchi@gmail.com>
2024-10-16 06:45:34 +0200

    Add tests for delete url

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/alicebob/miniredis/v2"
 	my_redis "github.com/mikandro/url_shortener/internal/redis"
+	"github.com/redis/go-redis/v9"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -34,7 +35,7 @@ func TestShortenUrl(t *testing.T) {
 	handler := &UrlHandler{RedisClient: redisClient}
 
 	reqBody := `{"url": "https://example.com"}`
-	req := httptest.NewRequest(http.MethodPost, "/shorten", strings.NewReader(reqBody))
+	req := httptest.NewRequest(http.MethodPost, "/url", strings.NewReader(reqBody))
 	req.Header.Set("Content-Type", "application/json")
 	w := httptest.NewRecorder()
 
@@ -68,12 +69,47 @@ func TestRedirect(t *testing.T) {
 
 	req := httptest.NewRequest(http.MethodGet, "/"+shortCode, nil)
 	w := httptest.NewRecorder()
-
 	handler.RedirectUrl(w, req)
 
 	res := w.Result()
 	defer res.Body.Close()
 
-	// Check that the response is a redirect
 	assert.Equal(t, http.StatusFound, res.StatusCode)
+
+	location := res.Header.Get("Location")
+	assert.Equal(t, longURL, location)
+}
+
+func TestDeleteShortUrl(t *testing.T) {
+	redisClient, teardown := setupTestRedis()
+	defer teardown()
+
+	handler := &UrlHandler{RedisClient: redisClient}
+
+	shortCode := "abcd1234"
+	longURL := "https://example.com"
+	ctx := context.Background()
+	err := redisClient.RedisClient.Set(ctx, shortCode, longURL, 0).Err()
+	assert.NoError(t, err)
+
+	req := httptest.NewRequest(http.MethodDelete, "/"+shortCode, nil)
+	w := httptest.NewRecorder()
+
+	handler.DeleteUrl(w, req)
+
+	res := w.Result()
+	defer res.Body.Close()
+
+	assert.Equal(t, http.StatusOK, res.StatusCode)
+
+	var responseBody map[string]string
+	err = json.NewDecoder(res.Body).Decode(&responseBody)
+	assert.NoError(t, err)
+
+	message, ok := responseBody["message"]
+	assert.True(t, ok)
+	assert.Equal(t, "Short URL deleted successfully", message)
+
+	_, err = redisClient.RedisClient.Get(ctx, shortCode).Result()
+	assert.Equal(t, redis.Nil, err)
 }
